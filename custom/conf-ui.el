7499a1812ecf5f6e55abd3229261b91b34939456
Eugene Sun <eug.sun@gmail.com>
2020-04-23 13:59:07 -0400

    A coherent version of the theme loading code

--- custom/conf-ui.el
+++ custom/conf-ui.el
@@ -14,25 +14,18 @@
 (setq theme-autoswitch/sync-timer 300)
 (if (and theme-autoswitch (display-graphic-p))
     (progn
-      (setq current-theme theme-autoswitch/dark-theme)
       (defun sync-theme-with-time ()
         (setq theme-autoswitch/hour (string-to-number (substring (current-time-string) 11 13)))
         (if (member theme-autoswitch/hour (number-sequence theme-autoswitch/day-start-hour theme-autoswitch/day-end-hour))
             (setq theme-autoswitch/now theme-autoswitch/light-theme)
           (setq theme-autoswitch/now theme-autoswitch/dark-theme))
-        (if (or (not (boundp 'current-theme)) (eq theme-autoswitch/now current-theme))
-            nil
+        (unless (and (boundp 'current-time) (eq theme-autoswitch/now current-theme))
           (progn
             (setq current-theme theme-autoswitch/now)
-            (load-theme theme-autoswitch/now t)
-            )
-          )
-        )
-      (run-with-timer 0 theme-autoswitch/sync-timer #'sync-theme-with-time)
-      )
-  (load-theme theme-autoswitch/dark-theme t)
-  )
-(load-theme theme-autoswitch/now t)
+            (load-theme theme-autoswitch/now t))))
+      (sync-theme-with-time)
+      (run-with-timer 0 theme-autoswitch/sync-timer #'sync-theme-with-time))
+  (load-theme theme-autoswitch/dark-theme t))
 
 ;; -- Chinese
 (when (member "WenQuanYi Micro Hei" (font-family-list))
